747605134e71c76f4d9da0b648ef024d586187b8
GayCookie <contact@gaycookie.dev>
2022-10-14 09:39:17 +0200

    Changed indents to spaces(2)

--- scripts/5_Mission/AutoRunMission.c
+++ scripts/5_Mission/AutoRunMission.c
@@ -1,53 +1,53 @@
 modded class MissionGameplay {
-	bool autoRunState;
+  bool autoRunState;
 
-	override void OnUpdate(float timeslice) {
-		super.OnUpdate(timeslice);
+  override void OnUpdate(float timeslice) {
+    super.OnUpdate(timeslice);
 
-		if (GetGame().GetUIManager().GetMenu() == NULL) {
-			Input input = GetGame().GetInput();
-			PlayerBase player = PlayerBase.Cast(GetGame().GetPlayer());
+    if (GetGame().GetUIManager().GetMenu() == NULL) {
+      Input input = GetGame().GetInput();
+      PlayerBase player = PlayerBase.Cast(GetGame().GetPlayer());
 
-			if (input.LocalPress("QSEnableAutoRun")) {
-				autoRunState = !autoRunState;
+      if (input.LocalPress("QSEnableAutoRun")) {
+        autoRunState = !autoRunState;
 
-				if (autoRunState) updateMovement(player, 3, 1);
-				else updateMovement(player, 0, 0);
-				return
-			}
+        if (autoRunState) updateMovement(player, 3, 1);
+        else updateMovement(player, 0, 0);
+        return
+      }
 
-			if (input.LocalPress("QSEnableAutoWalk")) {
-				autoRunState = !autoRunState;
+      if (input.LocalPress("QSEnableAutoWalk")) {
+        autoRunState = !autoRunState;
 
-				if (autoRunState) updateMovement(player, 1, 1);
-				else updateMovement(player, 0, 0);
-				return
-			}
+        if (autoRunState) updateMovement(player, 1, 1);
+        else updateMovement(player, 0, 0);
+        return
+      }
 
-			if (input.LocalPress("QSEnableAutoJog")) {
-				autoRunState = !autoRunState;
-				
-				if (autoRunState) updateMovement(player, 2, 1);
-				else updateMovement(player, 0, 0);
-				return
-			}
+      if (input.LocalPress("QSEnableAutoJog")) {
+        autoRunState = !autoRunState;
 
-			if (input.LocalPress("UAMoveForward") || input.LocalPress("UAMoveBack") || input.LocalPress("UAMoveLeft") || input.LocalPress("UAMoveRight")) {
-				if (autoRunState) {
-					autoRunState = false;
-					updateMovement(player, 0, 0)
-				}
-			}
-		}
-	}
+        if (autoRunState) updateMovement(player, 2, 1);
+        else updateMovement(player, 0, 0);
+        return
+      }
 
-	void updateMovement(PlayerBase player, int speed, int angle) {
-		player.GetInputController().OverrideMovementSpeed(autoRunState, speed);
-		player.GetInputController().OverrideMovementAngle(autoRunState, angle);
+      if (input.LocalPress("UAMoveForward") || input.LocalPress("UAMoveBack") || input.LocalPress("UAMoveLeft") || input.LocalPress("UAMoveRight")) {
+        if (autoRunState) {
+          autoRunState = false;
+          updateMovement(player, 0, 0)
+        }
+      }
+    }
+  }
 
-		if (GetGame().IsMultiplayer()) {
-			auto params = new Param3<bool, int, int>(autoRunState, speed, angle);
-			GetGame().RPCSingleParam(player, -77777, params, true);
-		}
-	}
+  void updateMovement(PlayerBase player, int speed, int angle) {
+    player.GetInputController().OverrideMovementSpeed(autoRunState, speed);
+    player.GetInputController().OverrideMovementAngle(autoRunState, angle);
+
+    if (GetGame().IsMultiplayer()) {
+      auto params = new Param3<bool, int, int>(autoRunState, speed, angle);
+      GetGame().RPCSingleParam(player, -77777, params, true);
+    }
+  }
 }
